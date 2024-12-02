952abcc37cd3ee31494234746d44f29d24e62b66
Future-Internet-Research-Lab-TJU <zli@tju.edu.cn>
2024-12-02 16:52:14 +0800

    Update heavy_change.cpp

--- heavy_change.cpp
+++ heavy_change.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
 #define Guide_Candidate_Mem (50 * 1024)
 #define BUCKET_NUM ((TOT_MEM_IN_BYTES - Guide_Candidate_Mem) / 8) 
-#define TOT_MEM_IN_BYTES (350 * 1024)   //定义分配的内存大小600KB
+#define TOT_MEM_IN_BYTES (350 * 1024)   
 
 
 vector<string> traces[END_FILE_NO - START_FILE_NO +1];
@@ -47,7 +47,7 @@ void ReadInTraces(const char *trace_prefix)
 		}
         fin.close(); 
 		//printf("Successfully read in %s, %ld packets, %ld flows\n", datafileName, traces[datafileCnt - 1].size(),Item[datafileCnt-1].size());
-        cout << "包数" << traces[datafileCnt - 1].size() << "  " << "流数" << Item[datafileCnt-1].size() << endl;
+        cout << "packets" << traces[datafileCnt - 1].size() << "  " << "flows" << Item[datafileCnt-1].size() << endl;
 	}
 	printf("\n");
 }
@@ -57,33 +57,30 @@ void heavy_change_test()
     AGCSketch<BUCKET_NUM, TOT_MEM_IN_BYTES> * agc1 = NULL;
     AGCSketch<BUCKET_NUM, TOT_MEM_IN_BYTES> * agc2 = NULL;
     
-    //第一个sketch
+
 	    agc1 = new AGCSketch<BUCKET_NUM, TOT_MEM_IN_BYTES>();      
 	    for (auto key:traces[0])  
         {	
             agc1->insert(key.c_str(), 1);          
 		}
         
-        //第二个sketch
+
         agc2 = new AGCSketch<BUCKET_NUM, TOT_MEM_IN_BYTES>();
         for (auto key:traces[1])  
         {	
             agc2->insert(key.c_str(), 1);          
 		}
 
-		//存储所有流在两个数据集下的真实差值
         unordered_map<string, int> answer;
         unordered_map<string, int> real;
 
         for (auto it:Item[0]){
-            (answer)[it.first] += it.second; //将trace1中的流放入到answer中
+            (answer)[it.first] += it.second;
         }
         for (auto it:Item[1]){
-            (answer)[it.first] -= it.second; //直接把trace2放入到answer中时做出差值记录，最终answer中存储的是所有流的差值
+            (answer)[it.first] -= it.second; 
         }
         
-
-        //统计总的差值作为判断的阈值
         int threshold = 0;
         for(unordered_map<string, int>::iterator it = answer.begin(); it != answer.end(); it++){
             threshold += abs(it->second) * 0.05 / 100;
@@ -92,7 +89,7 @@ void heavy_change_test()
         int tot_real = 0;
         for (unordered_map<string, int>::iterator it = answer.begin(); it != answer.end(); it++){
             if(abs(it->second) >= threshold){
-                tot_real++;  //RR的分母，真实的heavy change值
+                tot_real++;  
                 (real)[it->first] = it->second;
             }
         }
@@ -101,17 +98,17 @@ void heavy_change_test()
         unordered_map<string, int> est;
         
         for(auto it : Item[0]){
-            (heavy_change)[it.first] = agc1->query(it.first.c_str()) - agc2->query(it.first.c_str());  //将查询得到的差值放入到heavy change中
+            (heavy_change)[it.first] = agc1->query(it.first.c_str()) - agc2->query(it.first.c_str()); 
         }
 
         for(auto it : Item[1]){
-            (heavy_change)[it.first] = agc1->query(it.first.c_str()) - agc2->query(it.first.c_str());  //将查询得到的差值放入到heavy change中
+            (heavy_change)[it.first] = agc1->query(it.first.c_str()) - agc2->query(it.first.c_str()); 
         }
 
         int tot_report = 0;
         for (unordered_map<string, int>::iterator it = heavy_change.begin(); it != heavy_change.end(); it++){
             if(abs(it->second) >= threshold){
-                tot_report++;  //RR的分母，真实的heavy change值
+                tot_report++; 
                 (est)[it->first] = it->second;
             }
         }
@@ -120,8 +117,6 @@ void heavy_change_test()
         double ARE = 0, avg_PR = 0, avg_RR = 0, avg_ARE = 0, avg_F1 = 0;
 
         for (unordered_map<string, int>::iterator it = est.begin(); it !=  est.end(); it++){
-            //PR的分母，测量的heavy change值
-                //遍历it，查找answer中是否存在相同key
             for(unordered_map<string, int>::iterator iter = real.begin(); iter !=  real.end(); iter++){
                 if(iter->first == it->first ){
                     hit++;
